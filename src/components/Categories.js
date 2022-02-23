0dd5ab5ed6e4fe344c153d91dc8e4ba242a7b6fc
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-02-23 15:05:56 +0200

    Fix linter errors

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,7 +3,7 @@ import './categories.css';
 import React from 'react';
 
 const Cat = () => (
-  <div className="books-container">
+  <div className="cat-container">
     <h2>Categories</h2>
   </div>
 );
