88d1b0b6e442db2b3a9eb56c0d1801f3b86b802d
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-02-23 14:27:22 +0200

    Set routes and links for the app

---
+++ src/components/Books.js
@@ -0,0 +1,11 @@
+import './books.css';
+
+import React from 'react';
+
+const Books = () => (
+  <div className="books-container">
+    <h2>Books</h2>
+  </div>
+);
+
+export default Books;
---
+++ src/components/Categories.js
@@ -0,0 +1,11 @@
+import './categories.css';
+
+import React from 'react';
+
+const Cat = () => (
+  <div className="books-container">
+    <h2>Categories</h2>
+  </div>
+);
+
+export default Cat;
