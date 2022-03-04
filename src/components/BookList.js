3cbb195c15e1e39b0bcee44428aea0be45225130
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-03-04 13:27:56 +0200

    solve reviewer notes

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -17,13 +17,23 @@ const Booklist = () => {
   const handleAddBook = (e) => {
     e.preventDefault();
 
-    dispatch(
-      addBook({
-        item_id: uuidv4(),
-        title: title.current.value,
-        category: category.current.value,
-      }),
+    const book = {
+      item_id: uuidv4(),
+      title: title.current.value,
+      category: category.current.value,
+    };
+    fetch(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hU7hRqGMmMkUTHelAE4I/books',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(book),
+      },
     );
+
+    dispatch(addBook(book));
     title.current.value = '';
     category.current.value = '';
   };
