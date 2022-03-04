fcdb632638317c3f802d6669485c60a0d9a7f962
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-03-04 23:28:44 +0200

    add styles

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,52 +1,16 @@
 import './booklist.css';
 
-import { useRef } from 'react';
-import { useDispatch } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
 import Books from './Books';
 import Form from './Form';
 import ErrorBoundary from '../utils/ErrorBoundary';
-import { addBook } from '../redux/books/books';
 
-const Booklist = () => {
-  const title = useRef();
-  const category = useRef();
-
-  const dispatch = useDispatch();
-
-  const handleAddBook = (e) => {
-    e.preventDefault();
-
-    const book = {
-      item_id: uuidv4(),
-      title: title.current.value,
-      category: category.current.value,
-    };
-
-    fetch(
-      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hU7hRqGMmMkUTHelAE4I/books',
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(book),
-      },
-    );
-
-    dispatch(addBook(book));
-    title.current.value = '';
-    category.current.value = '';
-  };
-
-  return (
-    <div className="booklist-container">
-      <ErrorBoundary>
-        <Books />
-        <Form title={title} category={category} addBook={handleAddBook} />
-      </ErrorBoundary>
-    </div>
-  );
-};
+const Booklist = () => (
+  <div className="booklist-container">
+    <ErrorBoundary>
+      <Books />
+      <Form />
+    </ErrorBoundary>
+  </div>
+);
 
 export default Booklist;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,81 @@
-import React from 'react';
+import './form.css';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidc4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
-const Form = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const { addBook, title, category } = props;
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const book = {
+      item_id: uuidc4(),
+      title: `${title.trim()} , ${author.trim()}`,
+      category,
+    };
+
+    fetch(
+      'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/hU7hRqGMmMkUTHelAE4I/books',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(book),
+      }
+    );
+
+    dispatch(addBook(book));
+
+    setTitle('');
+    setCategory('');
+    setAuthor('');
+  };
+
   return (
-    <form className="book-container">
-      <input type="text" placeholder="title" ref={title} />
-      <input type="text" placeholder="auther" ref={category} />
-      <button type="submit" onClick={addBook}>
-        submit
-      </button>
-    </form>
+    <section className="form" onSubmit={handleSubmit}>
+      <h3>ADD NEW BOOK</h3>
+      <form>
+        <input
+          type="text"
+          placeholder="Book title"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <select
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          required
+        >
+          <option value="" disabled>
+            Category
+          </option>
+          <option value="Fantasy">Fantasy</option>
+          <option value="Science Fiction">Science Fiction</option>
+          <option value="Dystopian">Dystopian</option>
+          <option value="Adventure">Adventure</option>
+          <option value="Romance">Romance</option>
+          <option value="Detective & Mystery">Detective & Mystery</option>
+          <option value="Horror">Horror</option>
+          <option value="Thriller">Thriller</option>
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </section>
   );
 };
 
